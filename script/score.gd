efdce9d1ec9f08006105aeb9d5ddc89e0c8aee88
Hailey0930 <hyerin0930@gmail.com>
2024-10-17 23:05:53 +0900

    Feat : add score text

---
+++ script/score.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@onready var 점수 = $Label
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	점수.text = "점수 : " + str(Global.점수)
